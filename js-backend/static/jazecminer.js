4d4bbb91244897bf9bb1a9b7ef231fbe3da1b230
Mtv Europe <mtve1927@gmail.com>
2017-07-26 13:05:57 +0000

    compiled files

--- js-backend/static/jazecminer.js
+++ js-backend/static/jazecminer.js
@@ -1916,7 +1916,7 @@ function _emscripten_asm_const_v(code) {
 
 STATIC_BASE = Runtime.GLOBAL_BASE;
 
-STATICTOP = STATIC_BASE + 205558800;
+STATICTOP = STATIC_BASE + 205558848;
 /* global initializers */  __ATINIT__.push();
 
 
@@ -1925,7 +1925,7 @@ memoryInitializer = Module["wasmJSMethod"].indexOf("asmjs") >= 0 || Module["wasm
 
 
 
-var STATIC_BUMP = 205558800;
+var STATIC_BUMP = 205558848;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 
